07c94ceb9eca01bda60a8f75791d082f91fc7f3c
Ranjit Chavan <61783587+ranjitpchavan@users.noreply.github.com>
2023-07-24 23:50:06 +0530

    first commit

--- practice.js
+++ practice.js
@@ -1,17 +1,31 @@
 function findMax(numbers) {
-    if (!Array.isArray(numbers) || numbers.length === 0) {
-      return undefined;
-    }
-    let maxNumber = numbers[0];
-  
-    for (let i = 1; i < numbers.length; i++) {
-      if (numbers[i] > maxNumber) {
-        maxNumber = numbers[i];
-      }
-    }
-    return maxNumber;
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return undefined;
   }
-  
-  const numbersArray = [10, 5, 20, 8, 15];
+  let maxNumber = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
+    }
+  }
+  return "Largest number is :  " + maxNumber+ "<br>";
+}
+const numbersArray = [10, 5, 20, 8, 15];
 const maxNumber = findMax(numbersArray);
 document.write(maxNumber);
+
+
+function calculateArea(length, width) {
+  if (typeof length !== 'number' || typeof width !== 'number' || length <= 0 || width <= 0) {
+    return undefined;
+  }
+
+  const area = length * width;
+  return "Area : "+ area;
+}
+
+const length = 10;
+const width = 5;
+const area = calculateArea(length, width);
+document.write (area);
