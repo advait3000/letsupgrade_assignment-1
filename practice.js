09bac6f0f9008a3d2eb8906e5e6cd706b97f7fe6
Ranjit Chavan <61783587+ranjitpchavan@users.noreply.github.com>
2023-07-24 23:05:07 +0530

    Code Added

---
+++ practice.js
@@ -0,0 +1,17 @@
+function findMax(numbers) {
+    if (!Array.isArray(numbers) || numbers.length === 0) {
+      return undefined;
+    }
+    let maxNumber = numbers[0];
+  
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > maxNumber) {
+        maxNumber = numbers[i];
+      }
+    }
+    return maxNumber;
+  }
+  
+  const numbersArray = [10, 5, 20, 8, 15];
+const maxNumber = findMax(numbersArray);
+console.log(maxNumber);
